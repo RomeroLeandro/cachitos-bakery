7c10dbe0ad1c0a86c7e12321fc982a23f3e1b2bf
Leandro Romero <romerodisind@gmail.com>
2023-06-22 17:31:25 -0300

    primeros elementos en howAs

--- src/components/howAsk/HowAsk.jsx
+++ src/components/howAsk/HowAsk.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import './HowAsk.css';
+import Modal from 'react-modal';
+import whatsapp from '../../assets/icons/whatsapp.svg'
+import '../whatsapp-icon/whatsappIcon.css';
 
 export const HowAsk = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div className='menu'>HowAsk</div>
+    <div >
+      <h2>¡Comunicate con nosotros!</h2>
+      <div>
+        <p>Escríbenos por WhatsApp</p>
+        <div>
+          {/* <img src={whatsapp} alt="WhatsApp Icon" className="whatsapp" /> */}
+          <div>
+            <a href="https://api.whatsapp.com/send?phone=541155755890" target="_blank" rel="noopener noreferrer">097265263</a>
+            <p>Y</p>
+            <a href="https://api.whatsapp.com/send?phone=541155755890" target="_blank" rel="noopener noreferrer">092090229</a>
+          </div>
+
+        </div>
+
+        <div>
+
+
+        </div>
+
+      </div>
+    </div >
   )
 }
