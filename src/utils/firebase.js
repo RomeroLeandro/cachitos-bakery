3fbf557b3cf39ddcc3eee42aad705f788d39502e
Ing-ed <emidovichi@gmail.com>
2023-07-21 08:03:10 -0300

    Revisar seguridad de firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,6 +8,16 @@ const firebaseConfig = {
   messagingSenderId: "541850060285",
   appId: "1:541850060285:web:d36a108fe2b5cae9494594"
 };
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+// };
+
+// console.log(process.env.REACT_APP_SALUDO)
 
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
