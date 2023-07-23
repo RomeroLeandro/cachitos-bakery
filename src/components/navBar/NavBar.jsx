f59919ad87044438f9e3179711ca055a4569034a
Ing-ed <emidovichi@+gmail.com>
2023-07-23 20:40:19 -0300

    Version estable

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -28,6 +28,7 @@ export const NavBar = () => {
         })
         setCant(c)
     },[lista])
+
     useEffect(() => {
         document.addEventListener('click', handleClickOutside);
         return () => {
@@ -35,38 +36,6 @@ export const NavBar = () => {
         };
     }, []);
 
-    // const links = [
-    //     {
-    //         id: 1,
-    //         link: "menu",
-    //         nombre: "menu"
-    //     },
-    //     {
-    //         id: 2,
-    //         link: "comopedir",
-    //         nombre: "Como pedir"
-    //     },
-    //     {
-    //         id: 3,
-    //         link: "novedades",
-    //         nombre: "Novedades"
-    //     },
-    //     {
-    //         id: 4,
-    //         link: "sobrenosotros",
-    //         nombre: "Sobre nosotros"
-    //     },
-    //     {
-    //         id: 5,
-    //         link: "contacto",
-    //         nombre: "Contacto"
-    //     },
-    //     {
-    //         id: 6,
-    //         link: "comunidad",
-    //         nombre: "Comunidad"
-    //     },
-    // ];
     return (
         <header>
             <Aside />
@@ -84,11 +53,13 @@ export const NavBar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" className="bi bi-cart3" viewBox="0 0 16 16">
                                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                             </svg>
+                            {cant > 0 &&
+                                <>
+                                    <div className="notification" />
+                                    <p>Pulsa aqui para ir al carrito</p>
+                                </>
+                            }
                         </Link>
-                        {cant > 0 
-                            ? <p>{cant}</p>
-                            :<></>    
-                        }
                         {isSearchVisible && (
                             <div className="search-dropdown">
                                 <div>
